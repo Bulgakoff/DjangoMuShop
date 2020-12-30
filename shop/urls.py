ad08ecbb4f55a31488b271b903e827715f772720
Alex <hlbu@yandex.ru>
2020-12-31 00:06:49 +0300

    shop/5

--- shop/urls.py
+++ shop/urls.py
@@ -1,10 +1,9 @@
-
 from django.contrib import admin
 from django.urls import path
 from mainapp import views as mainapp_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', mainapp_views.index),
-    path('products/', mainapp_views.products),
+    path('', mainapp_views.index, name='index'),
+    path('products/', mainapp_views.products, name='products'),
 ]
